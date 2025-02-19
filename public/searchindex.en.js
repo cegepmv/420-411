ad44f1610c484d18f7258ae6c42b6dcea0463df8
Dominic Legare <dominic.legare@collegemv.qc.ca>
2025-02-19 12:57:47 -0500

    Ajout premier site statique hugo

---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Interface humain-machine",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/420-411/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Interface humain-machine",
+    "uri": "/420-411/index.html"
+  },
+  {
+    "breadcrumb": "Interface humain-machine",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/420-411/tags/index.html"
+  }
+]
